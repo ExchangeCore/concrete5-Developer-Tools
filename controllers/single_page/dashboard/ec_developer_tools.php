45cea5a5ebee8811f8556cbaed808f71d046b381
Chris Hougard <hougardc@msoe.edu>
2014-12-16 02:45:14 -0600

    List namespaces

--- controllers/single_page/dashboard/ec_developer_tools.php
+++ controllers/single_page/dashboard/ec_developer_tools.php
@@ -5,17 +5,38 @@ use Concrete\Core\Page\Controller\DashboardPageController;
 use Concrete\Core\Support\ServiceProvider\MetadataGenerator;
 use Concrete\Core\Support\Symbol\SymbolGenerator;
 use Core;
-use Google_Auth_Exception;
-use Google_Client;
-use Google_Service_Analytics;
-use Exception;
 use Package;
 
 class EcDeveloperTools extends DashboardPageController
 {
     public function view()
     {
+        $applicationPrefixes = array();
+        $packagePrefixes = array();
 
+        $loaders = spl_autoload_functions();
+        foreach ($loaders as $loader) {
+            if (is_array($loader) && $loader[0] instanceof \Concrete\Core\Foundation\ModifiedPsr4ClassLoader) {
+                $reflection = new \ReflectionClass($loader[0]);
+                $property = $reflection->getProperty('prefixes');
+                $property->setAccessible(true);
+                $prefixes = $property->getValue($loader[0]);
+                $property->setAccessible(false);
+
+                foreach ($prefixes as $prefix) {
+                    if (substr($prefix[0], 0, 12) == 'Application\\') {
+                        $applicationPrefixes[$prefix[0]] = $prefix[1];
+                    } elseif (preg_match('/^(Concrete\\\\Package\\\\)([A-Za-z_-]+)(\\\\.*)/', $prefix[0], $matches)) {
+                        $dir = Core::make('helper/text')->uncamelcase($matches[2]);
+                        $path = str_replace($dir, '<em>package_handle</em>', $prefix[1]);
+                        $packagePrefixes[$matches[1] . '<em>package_handle</em>' . $matches[3]] = $path;
+                    }
+                }
+            }
+        }
+
+        $this->set('applicationPrefixes', $applicationPrefixes);
+        $this->set('packagePrefixes', $packagePrefixes);
     }
 
     public function symbols()
@@ -25,6 +46,7 @@ class EcDeveloperTools extends DashboardPageController
         $symbols = $generator->render();
         file_put_contents($pkg->getPackagePath() . '/__IDE_SYMBOLS__.php', $symbols);
         $this->set('message', t('A symbols file was generated.'));
+        $this->view();
     }
 
     public function metadata()
@@ -34,5 +56,6 @@ class EcDeveloperTools extends DashboardPageController
         $symbols = $generator->render();
         file_put_contents($pkg->getPackagePath() . '/.phpstorm.meta.php', $symbols);
         $this->set('message', t('A metadata file was generated.'));
+        $this->view();
     }
 }
--- single_pages/dashboard/ec_developer_tools.php
+++ single_pages/dashboard/ec_developer_tools.php
@@ -1,3 +1,55 @@
 <h3>Support Files</h3>
 <p><a href="<?= $view->action('symbols') ?>" class="btn btn-primary"><?= t('Generate Symbols File') ?></a> <?= t('A symbols file will help your IDE auto-complete classes that are aliased.') ?></p>
-<p><a href="<?= $view->action('metadata') ?>" class="btn btn-primary"><?= t('Generate PhpStorm/IntellJ IDEA Metadata File') ?></a> <?= t('A metadata file helps PhpStorm and IntelliJ auto-complete instances created using Core::make().') ?></p>
+<?php
+if (class_exists('\Concrete\Core\Support\ServiceProvider\MetadataGenerator')) {
+    ?>
+    <p><a href="<?= $view->action('metadata') ?>"
+          class="btn btn-primary"><?= t('Generate PhpStorm/IntellJ IDEA Metadata File') ?></a> <?= t('A metadata file helps PhpStorm and IntelliJ auto-complete instances created using Core::make().') ?>
+    </p>
+<?php
+}
+?>
+<hr />
+<h3>Application Namespaces</h3>
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>Namespace</th>
+        <th>Directory</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($applicationPrefixes as $prefix => $directory) {
+        ?>
+        <tr>
+            <td><?= $prefix ?></td>
+            <td><?= $directory ?></td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
+<hr />
+<h3>Package Namespaces</h3>
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>Namespace</th>
+        <th>Directory</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($packagePrefixes as $prefix => $directory) {
+        ?>
+        <tr>
+            <td><?= $prefix ?></td>
+            <td><?= $directory ?></td>
+        </tr>
+    <?php
+    }
+    ?>
+    </tbody>
+</table>
