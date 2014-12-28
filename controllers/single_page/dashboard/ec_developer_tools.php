fe5e30eb2e339675c24783f9a459271b795a78ec
Chris Hougard <chris@exchangecore.com>
2014-12-28 17:28:57 -0600

    Use file helper

--- controllers/single_page/dashboard/ec_developer_tools.php
+++ controllers/single_page/dashboard/ec_developer_tools.php
@@ -41,15 +41,19 @@ class EcDeveloperTools extends DashboardPageController
 
     public function supportFiles()
     {
+        $fh = Core::make('helper/file');
         $pkg = Package::getByHandle('ec_dev_tools');
         $generator = new SymbolGenerator();
         $symbols = $generator->render();
-        file_put_contents($pkg->getPackagePath() . '/__IDE_SYMBOLS__.php', $symbols);
+
+        $fh->clear($pkg->getPackagePath() . '/__IDE_SYMBOLS__.php');
+        $fh->append($pkg->getPackagePath() . '/__IDE_SYMBOLS__.php', $symbols);
 
         if (class_exists('\Concrete\Core\Support\Symbol\MetadataGenerator')) {
             $generator = new MetadataGenerator();
-            $symbols = $generator->render();
-            file_put_contents($pkg->getPackagePath() . '/.phpstorm.meta.php', $symbols);
+            $metadata = $generator->render();
+            $fh->clear($pkg->getPackagePath() . '/.phpstorm.meta.php');
+            $fh->append($pkg->getPackagePath() . '/.phpstorm.meta.php', $metadata);
         }
 
         $this->set('message', t('The support files were generated.'));
