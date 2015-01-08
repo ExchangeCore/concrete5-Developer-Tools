6f41daf2a615e7b46c0b33050cb89c048472a5ed
Christopher Hougard <chris@exchangecore.com>
2015-01-07 20:04:39 -0600

    Missing translate

--- single_pages/dashboard/ec_developer_tools.php
+++ single_pages/dashboard/ec_developer_tools.php
@@ -1,6 +1,6 @@
 <?php defined('C5_EXECUTE') or die('Access denied.'); ?>
 
-<h3>Support Files</h3>
+<h3><?= t('Support Files') ?></h3>
 <p>
     <?= t('Support files are used to provide auto-completion support for IDEs. Without these files your IDE may not
     recognize all classes. The support files also add class type detection for <code>Core::make()</code> usages.') ?>
